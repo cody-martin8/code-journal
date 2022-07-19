df981848b3e7553d7c7863cd0968dfb3393173b4
Cody Martin <12martincody@gmail.com>
2022-07-19 04:59:05 +0000

    Completed up to adding a nextEntryId to the object and incrementing the nextEntryId in data.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 var $photoUrl = document.querySelector('.photoUrl');
 var $entryImage = document.querySelector('.entry-image');
 
-$photoUrl.addEventListener('input', function (event) {
+$photoUrl.addEventListener('input', function inputImage(event) {
   event.preventDefault();
   $entryImage.setAttribute('src', event.target.value);
+});
+
+var $journalEntry = document.getElementById('journal-entry-form');
+
+$journalEntry.addEventListener('submit', function inputJournalEntry(event) {
+  event.preventDefault();
+  var journalEntry = {};
+  for (var i = 0; i < event.target.length - 1; i++) {
+    journalEntry[event.target[i].className] = event.target[i].value;
+  }
+  journalEntry.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  // console.log(journalEntry);
 });

349a187883fee24f67463aff7a68301cff0c298f
Cody Martin <12martincody@gmail.com>
2022-07-20 03:14:29 +0000

    Defined function for creating a DOM tree, created a loop that appended journal entries to the page.

--- js/main.js
+++ js/main.js
@@ -20,3 +20,65 @@ $journalEntry.addEventListener('submit', function inputJournalEntry(event) {
   $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   $journalEntry.reset();
 });
+
+/*
+<li class="journal-entry-item">
+  <div class="container">
+    <div class="row">
+      <div class="column-half">
+        <img src="" alt="">
+      </div>
+      <div class="column-half">
+        <h2>Star Lord</h2>
+        <p>Star-Lord</p>
+        <p></p>
+      </div>
+    </div>
+  </div>
+</li>
+*/
+
+function renderJournalEntry(entry) {
+  var listItem = document.createElement('li');
+  listItem.className = 'journal-entry-item';
+
+  var containerDiv = document.createElement('div');
+  containerDiv.className = 'container';
+  listItem.appendChild(containerDiv);
+
+  var rowDiv = document.createElement('div');
+  rowDiv.className = 'row';
+  containerDiv.appendChild(rowDiv);
+
+  var columnDiv = document.createElement('div');
+  columnDiv.className = 'column-half';
+  rowDiv.appendChild(columnDiv);
+
+  var columnDivTwo = document.createElement('div');
+  columnDivTwo.className = 'column-half';
+  rowDiv.appendChild(columnDivTwo);
+
+  var journalImage = document.createElement('img');
+  journalImage.setAttribute('src', entry.photoUrl);
+  journalImage.setAttribute('alt', entry.title);
+  columnDiv.appendChild(journalImage);
+
+  var journalTitle = document.createElement('h2');
+  journalTitle.textContent = entry.title;
+  columnDivTwo.appendChild(journalTitle);
+
+  var journalWriteUp = document.createElement('p');
+  journalWriteUp.textContent = entry.notes;
+  columnDivTwo.appendChild(journalWriteUp);
+
+  return listItem;
+}
+
+// test function with data.entries[i]
+
+window.addEventListener('DOMContentLoaded', function loadJournal() {
+  var journalList = document.querySelector('#journal-entry-list');
+  for (var i = 0; i < data.entries.length; i++) {
+    journalList.appendChild(renderJournalEntry(data.entries[i]));
+  }
+});
